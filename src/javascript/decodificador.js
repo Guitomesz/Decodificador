bb7600e50a98d2433d71edc2bc26011068b5e925
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-02-07 20:39:30 -0300

    botão de copiar adicionado. estilização necessária.

--- src/javascript/decodificador.js
+++ src/javascript/decodificador.js
@@ -2,17 +2,25 @@ var emptyFieldMessage = document.querySelector('.emptyFieldMessage');
 var textToTransform = document.querySelector('.texto');
 var showUncryptedArea = document.querySelector('.uncryptedArea');
 var showEncryptedArea = document.querySelector('.encryptedArea');
+// var showCopyBtns = document.querySelector('.copyBtns');
+var showCopyBtnEncrypt = document.querySelector('.copyBtnEncrypt');
+var showCopyBtnUncrypt = document.querySelector('.copyBtnUncrypt');
 var uncryptedTextArea = document.querySelector('.UncryptedText');
 var encryptedTextArea = document.querySelector('.EncryptedText');
-
+var originalText = '';
 
 function EncryptArea() {
-    var texto = document.querySelector('.texto').value.trim();
+    var texto = textToTransform.value.trim();
     if (texto !== '') {
+        // Armazenar o texto original
+        originalText = texto;
+
         EncryptText();
         emptyFieldMessage.style.display = 'none';
         showEncryptedArea.style.display = 'block';
         showUncryptedArea.style.display = 'none';
+        showCopyBtnEncrypt.style.display = 'block';
+
     } else {
         emptyFieldMessage.style.display = 'block';
         emptyFieldWarning(emptyFieldMessage);
@@ -20,24 +28,21 @@ function EncryptArea() {
 }
 
 function UncryptArea() {
-    var texto = document.querySelector('.texto').value.trim();
+    var texto = textToTransform.value.trim();
     if (texto !== '') {
         UncryptText();
         emptyFieldMessage.style.display = 'none';
         showEncryptedArea.style.display = 'none';
         showUncryptedArea.style.display = 'block';
+        showCopyBtnUncrypt.style.display = 'block';
     } else {
         emptyFieldMessage.style.display = 'block';
         emptyFieldWarning(emptyFieldMessage);
     }
 }
 
-function emptyFieldWarning(e) {
-    e.classList.add('pulsarAviso');
-}
-
 function EncryptText() {
-    var textoAtual = textToTransform.value.toLowerCase()
+    var textoAtual = originalText.toLowerCase()
         .normalize("NFD")
         .replace(/[^a-zA-Z\s]/g, "");
     var novoTexto = textoAtual.replace(/e/g, 'enter')
@@ -46,7 +51,6 @@ function EncryptText() {
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
 
-    textToTransform.value = novoTexto;
     encryptedTextArea.innerHTML = novoTexto;
 }
 
@@ -60,13 +64,15 @@ function UncryptText() {
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
 
-    textToTransform.value = novoTexto;
-    encryptedTextArea.innerHTML = novoTexto;
+    uncryptedTextArea.innerHTML = novoTexto;
+}
+
+function emptyFieldWarning(e) {
+    e.classList.add('pulsarAviso');
 }
 
 textToTransform.addEventListener('enter', function () {
     EncryptText();
-
 });
 
 textToTransform.addEventListener('input', function () {
@@ -75,13 +81,55 @@ textToTransform.addEventListener('input', function () {
         .normalize("NFD")
         .replace(/[^a-zA-Z\s]/g, "");
 
-        if (textToTransform.value != '') {
-            emptyFieldMessage.classList.remove('pulsarAviso');
-        }
-});
-
-navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-    if (result.state === "granted" || result.state === "prompt") {
-        /* write to the clipboard now */
+    if (textToTransform.value != '') {
+        emptyFieldMessage.classList.remove('pulsarAviso');
     }
 });
+
+function writeToClipboardEncrypt() {
+    var encryptedText = '';
+    if (showEncryptedArea.style.display === 'block') {
+        encryptedText = encryptedTextArea.textContent;
+    } else if (showUncryptedArea.style.display === 'block') {
+        encryptedText = textToTransform.value.trim();
+    }
+
+    if (encryptedText !== '') {
+        navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+            if (result.state === "granted" || result.state === "prompt") {
+                navigator.clipboard.writeText(encryptedText)
+                    .then(() => {
+                        console.log('Texto copiado para a área de transferência:', encryptedText);
+                    })
+                    .catch((error) => {
+                        console.error('Erro ao copiar o texto para a área de transferência:', error);
+                    });
+            }
+        });
+    }
+}
+
+function writeToClipboardUncrypt() {
+    var uncryptedText = '';
+    if (showUncryptedArea.style.display === 'block') {
+        uncryptedText = uncryptedTextArea.textContent;
+    } else if (showEncryptedArea.style.display === 'block') {
+        uncryptedText = textToTransform.value.trim();
+    }
+
+    if (uncryptedText !== '') {
+        navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+            if (result.state === "granted" || result.state === "prompt") {
+                navigator.clipboard.writeText(uncryptedText)
+                    .then(() => {
+                        console.log('Texto copiado para a área de transferência:', uncryptedText);
+                    })
+                    .catch((error) => {
+                        console.error('Erro ao copiar o texto para a área de transferência:', error);
+                    });
+            }
+        });
+    }
+}
+
+
